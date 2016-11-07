7b42c509df741eaca8ab5da8e2b65bee80eb409b
Daniel Kang <dkinsk@gmail.com>
2016-11-07 18:10:02 -0500

    nunjucks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 var port = 3000;
 var volleyball = require('volleyball')
+var nunjucks = require('nunjucks')
 
 app.listen(port, (request, respond) => {
 	console.log('server listening: ', port);
@@ -13,12 +14,29 @@ app.listen(port, (request, respond) => {
 // });
 
 app.use(volleyball)
+// app.use(nunjucks)
 
 app.get('/news', (request, respond, next) => {
 	respond.send('news')
 });
 
 app.get('/', (request, respond, next) => {
-	respond.send('welcome')
+	respond.render('index.html',locals)
 });
 
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+
+// nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
